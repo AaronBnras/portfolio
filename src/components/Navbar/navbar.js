e71d2e26c7efefcc65affc42909e32cabc044f06
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-08 00:33:33 +0300

    initial update

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,17 +2,22 @@ import React from "react";
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
 import logo from "../../assets/Amlogo.png"
 import './navbar.css';
+import { Link } from "react-scroll";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src = {logo} alt="logo" className="logo"/><span>Aaron</span><span>Maeda</span>
+            <div><img src = {logo} alt="logo" className="logo"/><span>Aaron</span><span>Maeda</span></div>
+            
 
             <div className="desktopMenu">
-
+                <Link className="desktopMenuListName">Home</Link>
+                <Link className="desktopMenuListName">What i do</Link>
+                <Link className="desktopMenuListName">Portfolio</Link>
+                <Link className="desktopMenuListName">Contacts</Link>
             </div>
             <button className="desktopMenubtn">
-                <EnvelopeIcon className="h-6 w-6 text-white fill-current" />
+                <EnvelopeIcon className="h-6 w-6 text-white fill-current icon" />
                 <span>Hire Me</span>
             </button>
         </nav>
