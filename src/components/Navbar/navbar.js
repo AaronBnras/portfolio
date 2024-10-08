128a44da11ab66450a6985a8cc8a52f754751980
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-08 15:01:12 +0300

    initial update

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import logo from "../../assets/Amlogo.png"
+import logo from "../../assets/AM_logo.png"
 import './navbar.css';
 import { Link } from "react-scroll";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div><img src = {logo} alt="logo" className="logo"/><span>Aaron</span><span>Maeda</span></div>
-            
+            <div className="flex items-baseline">
+                <img src={logo} alt="logo" className="logo mr-2" />
+                <div className="ml-1 text-4xl bg-clip-text text-transparent text-center lg:text-left bg-gradient-to-r from-pink-500 to-primary">
+                    <span>Aaron</span>
+                    <span>Maeda</span>
+                </div>
+            </div>
 
             <div className="desktopMenu">
                 <Link className="desktopMenuListName">Home</Link>
@@ -16,8 +21,8 @@ const Navbar = () => {
                 <Link className="desktopMenuListName">Portfolio</Link>
                 <Link className="desktopMenuListName">Contacts</Link>
             </div>
-            <button className="desktopMenubtn">
-                <EnvelopeIcon className="h-6 w-6 text-white fill-current icon" />
+            <button className="desktopMenubtn bg-gradient-to-r from-pink-500 to-primary">
+                <EnvelopeIcon className="h-6 w-6 icon" />
                 <span>Hire Me</span>
             </button>
         </nav>
