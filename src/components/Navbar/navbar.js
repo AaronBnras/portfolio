62617749731d9ac9b998d9e76b33479f93e89ce0
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-08 21:54:08 +0300

    initial update

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BriefcaseIcon } from "@heroicons/react/24/outline";
 import logo from "../../assets/AM_logo.png"
 import './navbar.css';
 import { Link } from "react-scroll";
@@ -15,15 +14,15 @@ const Navbar = () => {
                 </div>
             </div>
 
-            <div className="desktopMenu">
+            <div className="desktopMenu text-xl">
                 <Link className="desktopMenuListName">Home</Link>
                 <Link className="desktopMenuListName">What i do</Link>
                 <Link className="desktopMenuListName">Portfolio</Link>
                 <Link className="desktopMenuListName">Contacts</Link>
             </div>
             <button className="desktopMenubtn bg-gradient-to-r from-pink-500 to-primary">
-                <BriefcaseIcon className="h-6 w-6 icon" />
-                <span>Hire Me</span>
+                {/* <BriefcaseIcon className="h-6 w-6 icon" /> */}
+                <span className="text-lg">Hire Me</span>
             </button>
         </nav>
     )
