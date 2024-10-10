dfa42aac1e977a548d0ec94097092e1161130d99
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-10 16:02:26 +0300

    initial update

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -6,24 +6,25 @@ import { Link } from "react-scroll";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="flex items-baseline">
-                <img src={logo} alt="logo" className="logo mr-2" />
-                <div className="ml-1 text-4xl bg-clip-text text-transparent text-center lg:text-left bg-gradient-to-r from-pink-500 to-primary">
-                    <span className="font-ubuntu">Aaron</span>
-                    <span className="font-ubuntu">Maeda</span>
+            <div className="nav">
+                <div className="flex items-baseline">
+                    <img src={logo} alt="logo" className="logo mr-2" />
+                    <div className="ml-1 text-4xl bg-clip-text text-transparent text-center lg:text-left bg-gradient-to-r from-pink-500 to-primary">
+                        <span className="font-ubuntu">Aaron</span>
+                        <span className="font-ubuntu">Maeda</span>
+                    </div>
                 </div>
-            </div>
 
-            <div className="desktopMenu text-xl">
-                <Link className="desktopMenuListName">Home</Link>
-                <Link className="desktopMenuListName">What i do</Link>
-                <Link className="desktopMenuListName">Portfolio</Link>
-                <Link className="desktopMenuListName">Contacts</Link>
+                <div className="desktopMenu text-xl">
+                    <Link className="desktopMenuListName">Home</Link>
+                    <Link className="desktopMenuListName">What i do</Link>
+                    <Link className="desktopMenuListName">Portfolio</Link>
+                    <Link className="desktopMenuListName">Contacts</Link>
+                </div>
+                <button className="desktopMenubtn bg-gradient-to-r from-pink-500 to-primary">
+                    <span className="text-lg">Hire Me</span>
+                </button>
             </div>
-            <button className="desktopMenubtn bg-gradient-to-r from-pink-500 to-primary">
-                {/* <BriefcaseIcon className="h-6 w-6 icon" /> */}
-                <span className="text-lg">Hire Me</span>
-            </button>
         </nav>
     )
 }
--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -8,19 +8,47 @@ import Mainanance from '../../assets/maintain.png'
 const Skills = () => {
     return (
         <section id="skills">
-            <span className="skillTittle">
+            <span className="skillTitle text-5xl ml-1">
                 What i do 🤔
             </span>
-            <span className="skillDesc">
-                Here are the primary services I offer on both freelance and contract basis. Kindly have a look and let me know how I may serve you!
-            </span>
+
+            <p className="skillDesc">
+                I provide these essential services both under contract and as a freelancer. Kindly check them out and let me know how I can help!
+                <span className="w-48 h-[4px] bg-gradient-to-r from-pink-500 to-blue-500 inline-block rounded-lg line"></span>
+            </p>
+            
+
             <div className="skillBars">
                 <div className="skillBar">
                     <img src={WebApp} alt="WEB-Development" className="SkillBarImg" />
                     <div className="SkillBarText">
                         <h1>01.</h1>
                         <h2>Website Design and Development</h2>
-                        <p>I design and develop stunning modern websites and web apps to elevate your personal brand, business or corporate to a next level</p>
+                        <p>Creating cutting-edge, high-performing websites and online apps that boost enterprises, businesses, and personal brands to new heights while providing customized solutions that propel achievement is my area of expertise.</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={MobileApp} alt="WEB-Development" className="SkillBarImg" />
+                    <div className="SkillBarText">
+                        <h1>02.</h1>
+                        <h2>Mobile Application Development</h2>
+                        <p>I design and develop cutting-edge, high-performing mobile applications that give flawless user experiences on all platforms and help you grow your company, corporation, or personal brand.</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={Design} alt="WEB-Development" className="SkillBarImg" />
+                    <div className="SkillBarText">
+                        <h1>03.</h1>
+                        <h2>Graphics Design</h2>
+                        <p>Invest in high-quality graphic materials to effectively brand your business or yourself. I create cards, banners, posters, logos, and much more to match your company's or personal style.</p>
+                    </div>
+                </div>
+                <div className="skillBar">
+                    <img src={Mainanance} alt="WEB-Development" className="SkillBarImg" />
+                    <div className="SkillBarText">
+                        <h1>04.</h1>
+                        <h2>Web and Computer Maintenance</h2>
+                        <p>I provide comprehensive installation, configuration, troubleshooting, and maintenance services for computers and their related peripherals and devices, addressing both hardware and software requirements.</p>
                     </div>
                 </div>
 
