c1f308860cf4c2799941e3ae130d6c58b2736264
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-13 23:35:51 +0300

    initial update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/navbar.js';
 import Intro from './components/Intro/intro.js';
 import Skills from './components/Skills/skills.js';
+import Works from './components/Works/works.js';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Intro />
       <Skills />
+      <Works />
     </div>
   );
 }
---
+++ src/components/Works/works.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './works.css';
+
+const Works = () => {
+    return(
+        <div className="">
+            WorKs
+        </div>
+    )
+}
+
+export default Works
