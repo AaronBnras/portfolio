7cffb19a6f2b177cd16d77fda9e16be834bb37b1
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-18 04:52:57 +0300

    initial update

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -20,7 +20,7 @@ const Works = () => {
                     <div className="relative rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={webapp} alt="Web Application" className="w-full h-full object-fill" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
-                            <a href="https://github.com/AaronBnras/-HIMS-_Homless_Information_Management_System-with-Laravel-" target="_blank" className="no-underline">
+                            <a href="https://github.com/AaronBnras/-HIMS-_Homless_Information_Management_System-with-Laravel-" rel="noopener noreferrer" target="_blank" className="no-underline">
                                 <button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button>
                             </a>
                         </div>
@@ -29,7 +29,7 @@ const Works = () => {
                     <div className="relative rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={mobileapp} alt="Mobile Application" className="w-full h-full object-fill" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
-                            <a href="https://github.com/AaronBnras/my_Greenhouse_App" target="_blank" className="no-underline">
+                            <a href="https://github.com/AaronBnras/my_Greenhouse_App" target="_blank" rel="noopener noreferrer" className="no-underline">
                                 <button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button>
                             </a>
                         </div>
@@ -38,21 +38,27 @@ const Works = () => {
                     <div className="relative rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={graphics} alt="Logo" className="w-full h-full object-fill" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
-                            <a href="#" className="no-underline"><button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button></a>
+                            <a href="#" target="_blank" rel="noopener noreferrer" className="no-underline">
+                                <button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button>
+                            </a>
                         </div>
                     </div>
 
                     <div className="relative rounded-lg w-96 h-96 overflow-hidden  shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={graphics2} alt="Logo" className="w-full h-full object-fill" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
-                            <a href="#" className="no-underline"><button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button></a>
+                            <a href="#" target="_blank" rel="noopener noreferrer" className="no-underline">
+                                <button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button>
+                            </a>
                         </div>
                     </div>
 
                     <div className="relative rounded-lg w-96 h-96 overflow-hidden  shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={businesscard} alt="Business Card" className="w-full h-full object-cover" />
                         <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
-                            <a href="#" className="no-underline"><button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button></a>
+                            <a href={`${process.env.PUBLIC_URL}/Doc/Business Card (YSO).pdf`} target="_blank" rel="noopener noreferrer" className="no-underline">
+                                <button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button>
+                            </a>
                         </div>
                     </div>
                 </div>
