bcb4346255d9c19cda48be793b556c6afedd6b3a
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-18 04:13:31 +0300

    initial update

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -17,29 +17,43 @@ const Works = () => {
                     <span className="block w-48 h-[2px] bg-gradient-to-r from-pink-500 to-blue-500 rounded-lg mx-auto my-4"></span>
                 </p>
                 <div className="flex flex-wrap justify-center gap-8 mt-4">
-                    <div className="rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
+                    <div className="relative rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={webapp} alt="Web Application" className="w-full h-full object-fill" />
-                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
+                            <a href="https://github.com/AaronBnras/-HIMS-_Homless_Information_Management_System-with-Laravel-" target="_blank" className="no-underline">
+                                <button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button>
+                            </a>
+                        </div>
                     </div>
 
-                    <div className="rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
+                    <div className="relative rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={mobileapp} alt="Mobile Application" className="w-full h-full object-fill" />
-                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
+                            <a href="https://github.com/AaronBnras/my_Greenhouse_App" target="_blank" className="no-underline">
+                                <button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button>
+                            </a>
+                        </div>
                     </div>
 
-                    <div className="rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
+                    <div className="relative rounded-lg w-96 h-96 overflow-hidden shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={graphics} alt="Logo" className="w-full h-full object-fill" />
-                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
+                            <a href="#" className="no-underline"><button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button></a>
+                        </div>
                     </div>
 
-                    <div className="rounded-lg w-96 h-96 overflow-hidden  shadow-2xl group transition-transform transform hover:scale-105">
+                    <div className="relative rounded-lg w-96 h-96 overflow-hidden  shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={graphics2} alt="Logo" className="w-full h-full object-fill" />
-                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
+                            <a href="#" className="no-underline"><button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button></a>
+                        </div>
                     </div>
 
-                    <div className="rounded-lg w-96 h-96 overflow-hidden  shadow-2xl group transition-transform transform hover:scale-105">
+                    <div className="relative rounded-lg w-96 h-96 overflow-hidden  shadow-2xl group transition-transform transform hover:scale-105">
                         <img src={businesscard} alt="Business Card" className="w-full h-full object-cover" />
-                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 justify-center items-center flex">
+                            <a href="#" className="no-underline"><button className="justify-center rounded-lg px-4 py-2 border-white border-2">View</button></a>
+                        </div>
                     </div>
                 </div>
 
