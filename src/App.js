4a661b8d839743c817957cf9c6270c897049f249
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-18 16:43:51 +0300

    initial update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/navbar.js';
 import Intro from './components/Intro/intro.js';
 import Skills from './components/Skills/skills.js';
 import Works from './components/Works/works.js';
+import Contacts from './components/Contacts/contacts.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Intro />
       <Skills />
       <Works />
+      <Contacts />
     </div>
   );
 }
