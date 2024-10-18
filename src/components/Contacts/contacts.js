e42408d878eaf71999c32f6a9f69e01a34b15628
Aaron Maeda <60480047+AaronBnras@users.noreply.github.com>
2024-10-18 16:44:01 +0300

    initial commit

---
+++ src/components/Contacts/contacts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './contacts.css';
+
+const Contacts = () => {
+    return (
+        <div className="">
+            <section className="">
+                <h1 className="">Contacts</h1>
+            </section>
+        </div>
+    )
+}
+
+export default Contacts
